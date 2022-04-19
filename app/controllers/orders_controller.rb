21f281eb30d864d2d17a269630526ea2ff6e1b5a
Daniel Armbruster <danielgundam00@gmail.com>
2022-04-19 15:04:53 -0600

    sorry

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,14 +14,14 @@ class OrdersController < ApplicationController
     order.to_json
   end
     
-#   patch 'checkout/:id' do
-#     order = Order.find(params[:id])
-#     order.update(
-#       user_id: params[:user_id]
-#       product_id: params[:product_id]
-#     )
-#     order.to_json
-#   end
+  # patch 'checkout/:id' do
+  #   order = Order.find(params[:id])
+  #   order.update(
+  #     user_id: params[:user_id]
+  #     product_id: params[:product_id]
+  #   )
+  #   order.to_json
+  # end
     
   delete 'checkout/:id' do
     order = Order.find(params[:id])
